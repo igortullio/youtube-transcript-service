755d4292191567ce454d970dca231ee37ffd3d5b
Igor Túllio <igortullio@hotmail.com>
2025-12-24 13:03:22 -0300

    feat: inject new headers

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,23 @@ import { env } from './env/index.js'
 import { authMiddleware } from './middleware/auth.js'
 import { YoutubePlusProvider } from './providers/youtube-plus-provider.js'
 
+const originalFetch = global.fetch
+
+global.fetch = async (url: string | URL | Request, config?: RequestInit) => {
+  const newConfig = {
+    ...config,
+    headers: {
+      ...config?.headers,
+      'User-Agent':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+      'Accept-Language': 'en-US,en;q=0.9',
+      Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    },
+  }
+
+  return originalFetch(url, newConfig)
+}
+
 const app = express()
 
 app.use(helmet())
